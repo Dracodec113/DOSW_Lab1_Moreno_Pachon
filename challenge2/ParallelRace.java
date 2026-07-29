d67d7eaf467d7254a9c745557ba34a66e1ca5b4d
Jeronimo Moreno Herrera <jeronimo.moreno-h@mail.escuelaing.edu.co>
2026-07-29 17:58:37 -0500

    feat: Max value function and allValues added

--- challenge2/ParallelRace.java
+++ challenge2/ParallelRace.java
@@ -1,3 +1,23 @@
+import java.util.List;
+import java.util.function.Function;
+
 public class ParallelRace {
-    
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        ParallelRace race = new ParallelRace();
+        System.out.println("All values: " + race.findAllValues(nums));
+    }
+
+    private Integer findMaxValue(List<Integer> nums) {
+        Function<List<Integer>, Integer> max = (numbs) -> numbs.stream()
+        .max(Integer::compare)
+        .orElse(null);
+        Integer maxValue = max.apply(nums);
+        return maxValue;
+    }
+
+    private List<Integer> findAllValues(List<Integer> nums) {
+        return List.of(findMaxValue(nums));
+    }
 }
+
