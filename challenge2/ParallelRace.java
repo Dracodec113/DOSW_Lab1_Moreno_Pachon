258f0bb9696eba038e14ee2aa1fe8688273e5da4
itsvale.pp <dv.pachonpinzon@gmail.com>
2026-07-29 18:00:25 -0500

    Challenge 2 - lane 2

--- challenge2/ParallelRace.java
+++ challenge2/ParallelRace.java
@@ -1,3 +1,29 @@
+import java.util.List;
+
 public class ParallelRace {
-    
+
+    public Integer smallestNum(List<Integer> numbers){
+        Integer small = numbers.stream()
+        .min(Integer::compare)
+        .orElse(null);
+
+        return small;
+    }
+
+    public Integer totalNums(List<Integer> numbers){
+        Long total = numbers.stream()
+        .count();
+
+        return total.intValue();
+    }
+
+    public List<Integer> findAllValues(List<Integer> nums){
+        return List.of(smallestNum(nums), totalNums(nums));
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        ParallelRace race = new ParallelRace();
+        System.out.println("All values: " + race.findAllValues(nums));
+    }
 }
