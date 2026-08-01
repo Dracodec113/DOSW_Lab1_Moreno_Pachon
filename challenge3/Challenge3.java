996e6b8c239e95eb602b960e3ff6cb7e76d5d75e
itsvale.pp <dv.pachonpinzon@gmail.com>
2026-08-01 18:05:09 -0500

    feat: method that repeat and reverse

--- challenge3/Challenge3.java
+++ challenge3/Challenge3.java
@@ -7,8 +7,7 @@ public class Challenge3{
     public static void main(String args[]){
         Challenge3 c3 = new Challenge3();
         String text = "Hello world!";
-        System.out.println("The reverse of Hello world! is: " + c3.reverseText(text));
-        System.out.println(c3.repeatString(text, 3));
+        System.out.println(c3.repeatReverseString(text, 3));
     }
 
     public StringBuffer reverseText(String text){
@@ -22,5 +21,11 @@ public class Challenge3{
             result.append(" " + str);
         }
         return result.toString();
+    }
+
+    public String repeatReverseString(String str, int times){
+        String repeat = repeatString(str, times);
+        String reverse = "The reverse of the text is:" + reverseText(repeat).toString();
+        return repeat + "   " + reverse;
     }
 }
