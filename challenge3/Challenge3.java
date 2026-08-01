1a7f60ffac1dbc9126f94a3ceb5fc1b1dd01e839
itsvale.pp <dv.pachonpinzon@gmail.com>
2026-08-01 17:33:58 -0500

    feat: StringBuffer message that reverse the text

--- challenge3/Challenge3.java
+++ challenge3/Challenge3.java
@@ -1,1 +1,17 @@
- /// Challenge
+/*
+Challenge 3
+ */
+
+public class Challenge3{
+    
+    public static void main(String args[]){
+        Challenge3 c3 = new Challenge3();
+        String text = "Hello world!";
+        System.out.println("The reverse of Hello world! is: " + c3.reverseText(text));
+    }
+
+    public StringBuffer reverseText(String text){
+        StringBuffer sbf = new StringBuffer(text);
+        return sbf.reverse();
+    }
+}
