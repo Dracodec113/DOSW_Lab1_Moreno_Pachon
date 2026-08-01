c2fd941f6c5580c3eb38bccfbd8053918a1abae5
Jeronimo Moreno Herrera <jeronimo.moreno-h@mail.escuelaing.edu.co>
2026-08-01 17:37:14 -0500

    feat: repeat 3 times method

--- challenge3/Challenge3.java
+++ challenge3/Challenge3.java
@@ -1,1 +1,15 @@
- /// Challenge
+public class Challenge3 {
+    public static void main(String[] args){
+        String test = "Buenas tardes";
+        Challenge3 challenge = new Challenge3();
+        System.out.println(challenge.repeatString(test, 3));
+    }
+
+    public String repeatString(String str, int times){
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < times; i++){
+            result.append(" " + str);
+        }
+        return result.toString();
+    }
+}
