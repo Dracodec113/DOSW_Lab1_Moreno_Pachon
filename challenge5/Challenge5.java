f0b0c47d913c131df0c0d96ff0826a9dbca76b8e
itsvale.pp <dv.pachonpinzon@gmail.com>
2026-08-02 06:37:17 -0500

    feat: implement challenge 5 student B

--- challenge5/Challenge5.java
+++ challenge5/Challenge5.java
@@ -1,1 +1,25 @@
- /// Challenge
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * Challenge 5
+ */
+
+public class Challenge5{
+    public static void main(String args[]){
+        Challenge5 c5 = new Challenge5();
+        List<Integer> numbers = List.of(12, 3, 25, 10, 7, 30, 18, 4);
+
+        TreeSet newNums = c5.treeSetBattle(numbers);
+        newNums.forEach(n -> System.out.println("Number in the arena: " + n));
+    }
+
+    private TreeSet<Integer> treeSetBattle(List<Integer> numbers){
+        TreeSet<Integer> answer = numbers.stream()
+        .filter(n -> n % 5 != 0)
+        .collect(Collectors.toCollection(TreeSet::new));
+
+        return answer;
+    }
+}
