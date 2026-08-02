56daba85a83f54e2785adf40cd9b81cc78b2b895
itsvale.pp <dv.pachonpinzon@gmail.com>
2026-08-01 21:20:11 -0500

    feat: challenge 4 - student B

--- challenge4/Challenge4.java
+++ challenge4/Challenge4.java
@@ -1,1 +1,42 @@
- /// Challenge
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Challenge 4
+ */
+
+public class Challenge4{
+
+    public static void main(String args[]){
+        Challenge4 c4 = new Challenge4();
+        List<Map<String, Integer>> info = new ArrayList<>();
+        info.add(Map.of("silver", 8));
+        info.add(Map.of("ruby", 4));
+        info.add(Map.of("gold", 12));
+        info.add(Map.of("emerald", 6));
+        System.out.println(c4.writeAnswer(c4.organize(info)));
+    }
+
+    private Hashtable<String, Integer> organize(List<Map<String, Integer>> info){
+        Hashtable<String, Integer> table = info.stream()
+            .flatMap(m -> m.entrySet().stream())
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (initial, second) -> initial,
+                Hashtable::new
+            ));
+
+        return table;
+    }
+
+    private String writeAnswer(Hashtable<String, Integer> data){
+        String ans = data.entrySet().stream()
+            .map(n -> "Key: " + n.getKey() + " |  Value:" + n.getValue())
+            .collect(Collectors.joining("\n"));
+        return ans;
+    }
+}
