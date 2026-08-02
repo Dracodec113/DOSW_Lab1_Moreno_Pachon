cc992098c4dd1cb67ec40444bec91882a1bba6bb
Jeronimo Moreno Herrera <jeronimo.moreno-h@mail.escuelaing.edu.co>
2026-08-02 16:51:11 -0500

    feat: Added commands as per the requirements. Switch statement not used.

--- challenge6/Challenge6.java
+++ challenge6/Challenge6.java
@@ -1,1 +1,27 @@
- /// Challenge
+import java.util.HashMap;
+import java.util.Map;
+
+public class Challenge6 {
+
+    private final Map<String, Runnable> commands = new HashMap<>();
+    public static void main(String[] args) {
+        Challenge6 c6 = new Challenge6();
+        c6.executeCommand("GREET");
+        c6.executeCommand("DANCE");
+        c6.executeCommand("SING");
+        c6.executeCommand("FAREWELL");
+    }
+
+    public Challenge6() {
+        commands.put("GREET", () -> System.out.println("Greetings, traveler of time and code!"));
+        commands.put("FAREWELL", () -> System.out.println("May the bits be with you until the next mission."));
+        commands.put("SING", () -> System.out.println("01010101"));
+        commands.put("DANCE", () -> System.out.println("Spinning in party mode."));
+    }
+
+    public void executeCommand(String command) {
+        Runnable action = commands.get(command);
+        action.run();
+    }
+
+}
